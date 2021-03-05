f87f3b94cfc1ce3aea8da3254bbe06ebec7f3613
jarredbaird@gmail.com <jarredbaird@gmail.com>
2021-03-05 18:30:14 -0500

    real commit now

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 """Flask app for Cupcakes"""
 from flask import Flask, request, jsonify, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "llama_eggs"
+app.config['SECRET_KEY'] = "llama_eggs
+
 
 debug = DebugToolbarExtension(app)
 connect_db(app)
